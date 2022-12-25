20d117ab2524b03dbbd9dc8b2f7b26fa73738dd8
AviGautam1 <avifetian@gmail.com>
2022-12-25 07:31:07 -0800

    first commit

---
+++ Program-5.py
@@ -0,0 +1,12 @@
+# Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them.
+
+first_name = input("Enter Your First Name : " )
+last_name = input("Enter Your Last Name : " )
+print(last_name + "  " + first_name)
+
+'''
+output = 
+Enter Your First Name : Aviensh
+Enter Your Last Name : Gautam
+Gautam  Aviensh
+'''
