90f3a1f9ae53f800371c3adc26466484291b770d
AviGautam1 <avifetian@gmail.com>
2022-12-25 10:01:04 -0800

    first commit

---
+++ Program-9.py
@@ -0,0 +1,11 @@
+'''
+Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+
+exam_st_date = (01, 01, 2023)
+'''
+
+exam_st_date = (1, 1, 2023)
+print( "The examination will start from : %i / %i / %i"  %exam_st_date)         # %i used for integer
+
+
+# output = oThe examination will start from : 1 / 1 / 2023
