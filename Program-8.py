d74701f25a6e78a466b63fa127db5643cbc36506
AviGautam1 <avifetian@gmail.com>
2022-12-25 09:51:20 -0800

    first commit

---
+++ Program-8.py
@@ -0,0 +1,17 @@
+# Write a Python program to display the first and last colors from the List.
+
+cols_list = ["Red", "Green", "Blue", "Yellow"]
+
+print(cols_list[0])
+print(cols_list[-1])
+print("%s %s"%(cols_list[0], cols_list[-1]))        # %s is used to perform concatenation of strings together.
+print(cols_list[0], cols_list[-1])
+
+
+'''
+output = 
+Red
+Yellow
+Red Yellow
+Red Yellow
+'''
