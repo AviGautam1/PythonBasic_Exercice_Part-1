45b6baf9420c6fc2d33821910b9b56a82011add2
AviGautam1 <avifetian@gmail.com>
2022-12-25 07:01:05 -0800

    first commit

---
+++ Program-2.py
@@ -0,0 +1,20 @@
+# Write a Python program to get the Python version you are using.
+'''
+Note : 'sys' module provides access to some variables
+used or maintained by the interpreter and to functions
+that interact strongly with the interpreter.
+'''
+import sys
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
+
+
+'''
+output = 
+Python version
+3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)]
+Version info.
+sys.version_info(major=3, minor=11, micro=1, releaselevel='final', serial=0)
+'''
