235bceb83fbd8b2cf80b282f29795c2f0d5b9f11
AviGautam1 <avifetian@gmail.com>
2022-12-25 11:20:29 -0800

    first commit

---
+++ Program-14.py
@@ -0,0 +1,18 @@
+'''
+Write a Python program to get the difference between a given number,
+if the number is greater than given number return double the absolute difference.
+'''
+
+a = int(input("Please enter a number : " ))
+d = a - 17
+if a>17:
+    s = 2 * d
+    print(s)
+else:
+    print(d)
+
+'''
+output = 
+Please enter a number : 22
+10
+'''
