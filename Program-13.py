f52c7b0be20ed92fe4fc18f3f7649037df6a558c
AviGautam1 <avifetian@gmail.com>
2022-12-25 10:47:34 -0800

    first commit

---
+++ Program-13.py
@@ -0,0 +1,15 @@
+# Write a Python program to get the volume of a sphere.
+
+r = float(input("Enter Radius Of Sphere : " ))
+
+pi = 3.1415926535897931
+
+V = 4.0 / 3.0 * pi * r**3
+
+print('The volume of the sphere is: ', V)
+
+'''
+output = 
+Enter Radius Of Sphere : 8
+The volume of the sphere is:  2144.660584850632
+'''
