e8f48d1951b8723f7f38a5e67db69081b7e8c196
AviGautam1 <avifetian@gmail.com>
2022-12-25 23:14:33 -0800

    first commit

---
+++ Program-40.py
@@ -0,0 +1,25 @@
+# Write a Python program to check if a number is positive, negative or zero.
+
+num = float(input("Input a number: "))
+if num > 0:
+    print("It is positive number")
+elif num == 0:
+    print("It is Zero")
+else:
+    print("It is a negative number")
+
+
+'''
+output = 
+Case-1
+Input a number: 3
+It is positive number
+
+Case-2
+Input a number: -3
+It is a negative number
+
+Case-3
+Input a number: 0
+It is Zero
+'''
