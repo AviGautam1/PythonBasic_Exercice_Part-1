9c99bbb8d89352af4b5f50eeadd38e42c6e9ea0a
AviGautam1 <avifetian@gmail.com>
2022-12-25 22:23:14 -0800

    first commit

---
+++ Program-28.py
@@ -0,0 +1,15 @@
+# Write a Python program to solve (x + y) * (x + y).
+
+x = int(input("Enter Value Of x : " ))
+y = int(input("Enter Value Of y : " ))
+
+sum = x + y
+result = sum * sum
+print(result)
+
+'''
+output = 
+Enter Value Of x : 2
+Enter Value Of y : 2
+16
+'''
