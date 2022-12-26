6cecbe3df4b0f9545a51a748fd57b694ff9d8c41
AviGautam1 <avifetian@gmail.com>
2022-12-25 22:31:10 -0800

    first commit

---
+++ Program-30.py
@@ -0,0 +1,12 @@
+# Write a Python program to sum of the first n positive integers.
+
+n = int(input("Input a number: "))
+sum_num = (n * (n + 1)) / 2
+print("Sum of the first", n ,"positive integers:", sum_num)
+
+
+'''
+output = 
+Input a number: 6
+Sum of the first 6 positive integers: 21.0
+'''
