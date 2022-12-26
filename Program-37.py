0c7e5bdc52518e273625518b35ec6994d047a066
AviGautam1 <avifetian@gmail.com>
2022-12-25 22:59:55 -0800

    first commit

---
+++ Program-37.py
@@ -0,0 +1,12 @@
+# Write a Python program to concatenate N strings.
+
+list_of_colors = ['Red', 'White', 'Black']
+colors = ','.join(list_of_colors)
+print("All Colors: "+colors)
+print(type(colors))
+
+'''
+output = 
+All Colors: Red,White,Black
+<class 'str'>
+'''
