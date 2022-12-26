e1825de630a66b13209c73b1bcf34280ff3de86a
AviGautam1 <avifetian@gmail.com>
2022-12-25 23:02:14 -0800

    first commit

---
+++ Program-38.py
@@ -0,0 +1,6 @@
+# Write a Python program to calculate the sum of all items of a container (tuple, list, set, dictionary).
+
+s = sum([10,20,30])
+print("Sum of the container: ", s)
+
+# output = Sum of the container:  60
