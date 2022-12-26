a6503f9789368394f92c2db00170535fa509cc63
AviGautam1 <avifetian@gmail.com>
2022-12-25 23:35:51 -0800

    first commit

---
+++ Program-41.py
@@ -0,0 +1,14 @@
+# Write a Python program to remove the first item from a specified list.
+
+emp_name = ["avinesh", "sandeep", "priyanka", "shreyasi"]
+
+print("Before Removing ", emp_name)
+
+del emp_name[0]
+
+print("After Removing ", emp_name)
+
+'''
+Before Removing  ['avinesh', 'sandeep', 'priyanka', 'shreyasi']
+After Removing  ['sandeep', 'priyanka', 'shreyasi']
+'''
