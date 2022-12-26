6d81aa426f663a780f4d1de36ff42a2a38b1c46d
AviGautam1 <avifetian@gmail.com>
2022-12-25 23:07:32 -0800

    first commit

---
+++ Program-39.py
@@ -0,0 +1,15 @@
+# Write a Python program to swap two variables.
+
+# The %d operator is used as a placeholder to specify integer values, decimals, or numbers.
+a = 30
+b = 20
+print("\nBefore swap a = %d and b = %d" %(a, b))
+a, b = b, a
+print("\nAfter swaping a = %d and b = %d" %(a, b))
+
+'''
+output = 
+Before swap a = 30 and b = 20
+
+After swaping a = 20 and b = 30
+'''
